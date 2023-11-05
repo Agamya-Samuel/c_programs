97666d128385223d043856b7db11e85151ac4c5c
Agamya Samuel <48693970+Agamya-Samuel@users.noreply.github.com>
2023-11-06 00:26:26 +0530

    Create reverse_string.c
    - reverse of a String in C using char[] array.

---
+++ reverse_string.c
@@ -0,0 +1,20 @@
+// a program to create reverse of a string
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[100],rev[100];
+    int i,j,len;
+    printf("Enter the string: ");
+    scanf("%s",str);
+    len=strlen(str);
+    for(i=len-1,j=0;i>=0;i--,j++)
+    {
+        rev[j]=str[i];
+    }
+    rev[j]='\0';
+    printf("The reverse string is: %s\n",rev);
+    return 0;
+}
